77958bd8b61fe514f294cf8d3a227ca267aaeb61
Nishikanta Bhuyan <150238523+nishikanta01@users.noreply.github.com>
2024-02-24 13:36:50 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
-class Hello{
-  public static void main(String [] args){
-    System.out.println("Hello world");
-      }
+class Demo{
+	public static void main(String [] args){
+		System.out.println("Hello All");
+		System.out.println("Hello Jenkins");
+		System.out.println("Hello Git");
+	}
 }
